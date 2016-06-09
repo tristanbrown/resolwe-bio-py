9a9f9175b28c5058e902e24fe4e1981848f3c7ad
Jure Zmrzlikar <zmrzlikar.jure@gmail.com>
2016-06-09 14:07:00 +0200

    Unit tests for ResolweQuerry class

--- resdk/tests/unit/test_resolwequerry.py
+++ resdk/tests/unit/test_resolwequerry.py
@@ -4,18 +4,24 @@ Unit tests for resdk/resolwe.py file.
 # pylint: disable=missing-docstring, protected-access
 
 import unittest
+
 import six
+import slumber
 
 from mock import patch, MagicMock
 
 from resdk.resolwe import ResolweQuerry
 
-if six.PY2:
-    # pylint: disable=deprecated-method
-    unittest.TestCase.assertRegex = unittest.TestCase.assertRegexpMatches
-
 
 class TestResolweQuerry(unittest.TestCase):
+
+    @patch('resdk.resolwe.getattr')
+    @patch('resdk.resolwe.Resolwe')
+    @patch('resdk.resources.sample.Sample')
+    @patch('resdk.resolwe.ResolweQuerry', spec=True)
+    def test_init(self, resq_mock, resolwe_mock, resource_mock, getattr_mock):
+        getattr_mock.return_value = "blah"
+        ResolweQuerry.__init__(resq_mock, resolwe_mock, resource_mock)
 
     @patch('resdk.resolwe.ResolweQuerry', spec=True)
     def test_get(self, resq_mock):
@@ -28,6 +34,25 @@ class TestResolweQuerry(unittest.TestCase):
         data = ResolweQuerry.get(resq_mock, 40)
         self.assertEqual(data, "Some response")
 
+        # User provides wrong ID.
+        message = r'Id: .* does not exist or you dont have access permission.'
+        resq_mock.resource = MagicMock(side_effect=[slumber.exceptions.HttpNotFoundError(message)])
+        with six.assertRaisesRegex(self, slumber.exceptions.HttpNotFoundError, message):
+            ResolweQuerry.get(resq_mock, 12345)
+
+        # User provides correct slug.
+        resq_mock.api = MagicMock()
+        resq_mock.resolwe = MagicMock()
+        resq_mock.resource = MagicMock(return_value="Some response")
+        data = ResolweQuerry.get(resq_mock, "some-slug")
+        self.assertEqual(data, "Some response")
+
+        # User provides wrong slug.
+        message = r'Slug: .* does not exist or you dont have access permission.'
+        resq_mock.resource = MagicMock(side_effect=[IndexError(message)])
+        with six.assertRaisesRegex(self, IndexError, message):
+            ResolweQuerry.get(resq_mock, "some-slug")
+
     @patch('resdk.resolwe.ResolweQuerry', spec=True)
     def test_filter(self, resq_mock):
 
@@ -38,6 +63,11 @@ class TestResolweQuerry(unittest.TestCase):
         data = ResolweQuerry.filter(resq_mock, slug="some-slug")
         self.assertIsInstance(data, list)
         self.assertEqual(data[0], 12345)
+
+    @patch('resdk.resolwe.ResolweQuerry', spec=True)
+    def test_search(self, resq_mock):
+        with six.assertRaisesRegex(self, NotImplementedError, ""):
+            ResolweQuerry.search(resq_mock)
 
 if __name__ == '__main__':
     unittest.main()
