ccf7835a3a16e948b2e19ba1410151cc1deb9731
Jure Zmrzlikar <zmrzlikar.jure@gmail.com>
2016-07-27 19:37:56 +0200

    Fix resolwe.run docstring

--- resdk/resolwe.py
+++ resdk/resolwe.py
@@ -253,25 +253,17 @@ class Resolwe(object):
         in the tools argument. This scripts will be copied to the
         server automatically with SCP.
 
-        :param slug: Process slug (unique identifier)
-        :type slug: str
-        :param input: Input values
-        :type input: dict
-        :param descriptor: Descriptor values
-        :type descriptor: dict
-        :param descriptor_schema: Descriptor fields
-        :type descriptor_schema: list of dicts
-        :param collections: Default collections of Data object
-        :type collections: list of ints
-        :param data_name: Default name of Data object
-        :type data_name: string
-        :param src: Register process from source YAML file
-        :type src: str
-        :param tools: Process auxiliary scripts to upload
-        :type tools: list of str
+        :param str slug: Process slug (human readable unique identifier)
+        :param dict input: Input values
+        :param dict descriptor: Descriptor values
+        :param str descriptor_schema: A valid descriptor schema slug
+        :param list collections: Id's of collections into which data object should be included
+        :param str data_name: Default name of data object
+        :param str src: Path to YAML file with custom process definition
+        :param list tools: Paths to auxiliary scripts to upload
 
+        :return: data object that was just created
         :rtype: Data object
-
         """
         if ((descriptor and not descriptor_schema) or
                 (not descriptor and descriptor_schema)):
