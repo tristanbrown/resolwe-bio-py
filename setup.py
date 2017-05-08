e2026d46d464e5a55d979e0649893b475b6458ae
Domen Blenkuš <domen@blenkus.com>
2017-05-08 14:46:40 +0200

    Update suported versions of Python 3

--- setup.py
+++ setup.py
@@ -94,8 +94,9 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     keywords='bioinformatics resolwe bio pipelines dataflow django python sdk',
